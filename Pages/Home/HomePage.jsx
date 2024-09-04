e1cc6f17fe8ea8577898844f008af6b180f18145
Rahul Panchal <rm5901960@gmail.com>
2024-09-04 16:04:03 +0530

    Add TailwindCSS

---
+++ Pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+
+const HomePage = ()=>{
+    return(
+        <>
+            <div className="w-screen h-screen bg-red-300 flex flex-col justify-center items-center">
+                Home page
+            </div>
+        </>
+    )
+}
+
+export default HomePage
