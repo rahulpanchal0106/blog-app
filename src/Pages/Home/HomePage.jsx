a7a441ffeef80b42683c5297a37544ba6dacb040
Rahul Panchal <rm5901960@gmail.com>
2024-09-06 01:43:46 +0530

    Add  home Page

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,12 +1,30 @@
+import { Link } from "react-router-dom"
 import NavBar from "../../Components/NavBar/NavBar"
 
 const HomePage = ()=>{
     return(
         <>
             <NavBar/>
-            <div className="w-screen h-screen bg-red-300 flex flex-col justify-center items-center">
-                Home page
+        
+            <div className="w-screen h-screen bg-white flex flex-row justify-evenly items-center">
+                <div className="flex flex-col justify-left">
+                    <p className="w-full text-9xl px-2 py-4 ">
+                        Human
+                        <br />
+                        stories & ideas
+                    </p>
+                    <p className="w-full text-2xl px-2 py-4">
+                        A place to read, write, and deepen your understanding
+                    </p>
+                    <Link to="/login">
+                        <button type="submit" className="w-1/2 mt-5 flex flex-row items-center justify-evenly rounded-full border border-solid border-gray-400 px-5 py-3 border-black hover:bg-white bg-black text-white hover:text-black" >
+                            Start Reading
+                        </button>
+                    </Link>
+                </div>
+                <img src="/bg.svg" alt="" width={400} />
             </div>
+        
         </>
     )
 }
